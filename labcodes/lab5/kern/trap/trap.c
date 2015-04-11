6da7a971cb0f4e4ac4ad727ab398d7ec8c2072f7
Briefly <brieflyx@gmail.com>
2015-04-11 22:52:50 +0800

    update lab1 \& lab2 codes

--- labcodes/lab5/kern/trap/trap.c
+++ labcodes/lab5/kern/trap/trap.c
@@ -53,7 +53,7 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
-     /* LAB5 YOUR CODE */ 
+     /* LAB5 2012010685 */ 
      //you should update your lab1 code (just add ONE or TWO lines of code), let user app to use syscall to get the service of ucore
      //so you should setup the syscall interrupt gate in here
     extern uintptr_t __vectors[];
@@ -62,6 +62,7 @@ idt_init(void) {
         SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
     }
     SETGATE(idt[T_SWITCH_TOK], 0, GD_KTEXT, __vectors[T_SWITCH_TOK], DPL_USER);
+    SETGATE(idt[T_SYSCALL], 1, GD_KTEXT, __vectors[T_SYSCALL], DPL_USER);
     lidt(&idt_pd);
 
 }
@@ -227,16 +228,15 @@ trap_dispatch(struct trapframe *tf) {
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
          * (3) Too Simple? Yes, I think so!
          */
-        /* LAB5 YOUR CODE */
+        /* LAB5 2012010685 */
         /* you should upate you lab1 code (just add ONE or TWO lines of code):
          *    Every TICK_NUM cycle, you should set current process's current->need_resched = 1
          */
         ticks ++;
         if (!(ticks % TICK_NUM)) {
             print_ticks();
+            current -> need_resched = 1;
         }
-
-  
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
