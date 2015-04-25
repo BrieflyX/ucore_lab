a3a4e1e7a88ab3b771bd30969f76faffd0e8adcb
Briefly <brieflyx@gmail.com>
2015-04-25 19:31:14 +0800

    fix bugs in original RR code

--- labcodes/lab6/kern/schedule/default_sched.c
+++ labcodes/lab6/kern/schedule/default_sched.c
@@ -12,7 +12,6 @@ RR_init(struct run_queue *rq) {
 
 static void
 RR_enqueue(struct run_queue *rq, struct proc_struct *proc) {
-    assert(list_empty(&(proc->run_link)));
     list_add_before(&(rq->run_list), &(proc->run_link));
     if (proc->time_slice == 0 || proc->time_slice > rq->max_time_slice) {
         proc->time_slice = rq->max_time_slice;
@@ -23,7 +22,6 @@ RR_enqueue(struct run_queue *rq, struct proc_struct *proc) {
 
 static void
 RR_dequeue(struct run_queue *rq, struct proc_struct *proc) {
-    assert(!list_empty(&(proc->run_link)) && proc->rq == rq);
     list_del_init(&(proc->run_link));
     rq->proc_num --;
 }
