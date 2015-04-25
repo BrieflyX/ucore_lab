01f6f09824507aada838f561cffea362419a89da
Briefly <brieflyx@gmail.com>
2015-04-25 21:14:38 +0800

    just could run

--- labcodes/lab6/kern/process/proc.c
+++ labcodes/lab6/kern/process/proc.c
@@ -450,7 +450,7 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
         set_links(proc);
     }
     local_intr_restore(intr_flag);
-    nr_process++;
+    // nr_process++;
     wakeup_proc(proc);
     ret = proc -> pid;
 
